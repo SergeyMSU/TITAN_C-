1c23d490ce8b80d8c950569f3a856c5064f3d14a
SergeyMSU <korolkov.msu@mail.ru>
2025-09-01 23:15:53 +0300

    5.573

--- TITAN/source/TITAN.cpp
+++ TITAN/source/TITAN.cpp
@@ -29,7 +29,7 @@ int main()
     S1.Init_boundary_grans();
     cout << "C " << endl;
 
-    S1.Download_cell_parameters("parameters_0110.bin");   // 107
+    S1.Download_cell_parameters("parameters_promeg_113.bin");   // 107
     // 19 стартовая точка от которой две параллели с пикапами и без
     // 32 с пикапами
     // 62 включи TVD
@@ -75,7 +75,7 @@ int main()
     S1.Smooth_head_TS3();
 
 
-    for (int i = 1; i <= 3 * 12; i++) // 6 * 2
+    for (int i = 1; i <= 3 * 10; i++) // 6 * 2
     {
         auto start = std::chrono::high_resolution_clock::now();
         cout << "IIIII = " << i << endl;
