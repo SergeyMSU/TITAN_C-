7cb7063727209377bbd7e4653e87c32f059496db
SergeyMSU <korolkov.msu@mail.ru>
2025-09-10 23:38:16 +0300

    5.751

--- TITAN/source/TITAN.cpp
+++ TITAN/source/TITAN.cpp
@@ -29,7 +29,7 @@ int main()
     S1.Init_boundary_grans();
     cout << "C " << endl;
 
-    S1.Download_cell_parameters("parameters_0056.bin");   // 107   119
+    S1.Download_cell_parameters("parameters_0136.bin");   // 107   119
     //S1.Download_cell_parameters("parameters_0054.bin");   // 107
 
     // 19 стартовая точка от которой две параллели с пикапами и без
@@ -77,13 +77,9 @@ int main()
     S1.Smooth_head_TS3();
 
 
-    S1.Go(false, 40, 1); // 400   1
-    S1.Tecplot_print_all_gran_in_surface("TS");
-    S1.Tecplot_print_all_gran_in_surface("HP");
-    S1.Tecplot_print_all_gran_in_surface("BS");
 
 
-    for (int i = 1; i <= 3 * 6; i++) // 6 * 2
+    for (int i = 1; i <= 3 * 10; i++) // 6 * 2
     {
         auto start = std::chrono::high_resolution_clock::now();
         cout << "IIIII = " << i << endl;
@@ -124,7 +120,7 @@ int main()
     }
 
     //S1.Save_cell_parameters("parameters_0055.bin");
-    S1.Save_cell_parameters("parameters_0057.bin");
+    S1.Save_cell_parameters("parameters_0137.bin");
     //S1.Save_cell_pui_parameters("parameters_0026.bin");
 
     S1.Edges_create();
