e6da78b42292f1f78c842569ecf227aea4aa0be7
SergeyMSU <korolkov.msu@mail.ru>
2025-10-20 23:54:17 +0300

    6.641

--- TITAN/source/Setka.cpp
+++ TITAN/source/Setka.cpp
@@ -220,13 +220,13 @@ void Setka::Algoritm(short int alg)
 		zones_number.push_back(2); zones_n_koeff.push_back(1.0);
 		zones_number.push_back(1); zones_n_koeff.push_back(1.0);
 		zones_number.push_back(3); zones_n_koeff.push_back(1.0);
-		zones_number.push_back(5); zones_n_koeff.push_back(1.0);
+		/*zones_number.push_back(5); zones_n_koeff.push_back(1.0);
 		zones_number.push_back(7); zones_n_koeff.push_back(1.0);
 		zones_number.push_back(1); zones_n_koeff.push_back(1.0);
 		zones_number.push_back(2); zones_n_koeff.push_back(1.0);
 		zones_number.push_back(3); zones_n_koeff.push_back(1.0);
 		zones_number.push_back(4); zones_n_koeff.push_back(2.0);
-		zones_number.push_back(5); zones_n_koeff.push_back(1.0);
+		zones_number.push_back(5); zones_n_koeff.push_back(1.0);*/
 
 		short int ijij = 0;
 		for (const auto& zone_play : zones_number)
