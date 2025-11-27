d529122045a0fef71f692d21f98251f32760a07f
SergeyMSU <korolkov.msu@mail.ru>
2025-11-27 19:59:25 +0300

    7.862

--- TITAN/source/Setka_algoritm.cpp
+++ TITAN/source/Setka_algoritm.cpp
@@ -869,6 +869,8 @@ void Setka::Algoritm(short int alg, Setka* Smain)
 		zones_number.push_back(4); zones_n_koeff.push_back(1.0);
 		zones_number.push_back(6); zones_n_koeff.push_back(1.0);
 		zones_number.push_back(3); zones_n_koeff.push_back(1.0);
+		zones_number.push_back(5); zones_n_koeff.push_back(1.0);
+		zones_number.push_back(7); zones_n_koeff.push_back(1.0);
 
 
 
