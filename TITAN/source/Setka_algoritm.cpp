68204f95990f417766bf8e5076a26643f2c08c11
SergeyMSU <korolkov.msu@mail.ru>
2026-01-28 00:43:34 +0300

    7.793

--- TITAN/source/Setka_algoritm.cpp
+++ TITAN/source/Setka_algoritm.cpp
@@ -159,7 +159,7 @@ void Setka::Algoritm(short int alg, Setka* Smain)
 		this->Smooth_head_TS3();
 
 
-		for (int i = 1; i <= 12 * 1; i++) // 6 * 2   12 * 5
+		for (int i = 1; i <= 12 * 8; i++) // 6 * 2   12 * 5
 		{
 			auto start = std::chrono::high_resolution_clock::now();
 			cout << "IIIII = " << i << endl;
