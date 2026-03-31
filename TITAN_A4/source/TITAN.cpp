8257aa822b78887e825a9832e981a1891bd55bcd
Sergey_IKI <korolkov.msu@mail.ru>
2026-03-31 16:08:04 +0300

    8.33
    Пытаюсь установить с источниками от пикапов

--- TITAN_A4/source/TITAN.cpp
+++ TITAN_A4/source/TITAN.cpp
@@ -81,9 +81,10 @@ int main()
     //  
 
     //S1.Download_cell_parameters("parameters_0081.bin");  // Стартовали согласование с 81  
-    S1.Download_cell_parameters("parameters_0083.bin");    
+    S1.Download_cell_parameters("parameters_0084.bin");    
+
     //S1.Download_cell_parameters("parameters_0219.bin");    
-    // S1.Download_cell_parameters("parameters_promeg_1112.bin");
+    //S1.Download_cell_parameters("parameters_promeg_1130.bin");
     //S1.Download_cell_parameters("parameters_promeg_1118.bin");
     //S1.Download_cell_parameters("parameters_promeg_1112.bin");
 
@@ -236,7 +237,7 @@ int main()
     }
 
 
-    S1.Save_cell_parameters("parameters_0084.bin");
+    S1.Save_cell_parameters("parameters_0085.bin");
     //S1.Save_cell_parameters("parameters_0138.bin");
     //S1.Save_cell_pui_parameters("parameters_0026.bin");
 
