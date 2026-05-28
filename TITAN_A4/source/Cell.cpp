ab4b7bb449578fd0c9a231b77db3d7858ab436ea
Sergey_IKI <korolkov.msu@mail.ru>
2026-05-28 17:08:06 +0300

    8.529

--- TITAN_A4/source/Cell.cpp
+++ TITAN_A4/source/Cell.cpp
@@ -2040,7 +2040,7 @@ void Cell::MK_Add_pui_source(MK_particle& P, const double& wr, const double& nu_
 	bool first_pui = false;       // Первый раз запускаем пуи? Это надо, когда пуи ещё нет в распределениях, но источники надо посчитать
 	// zone = 1, 2, 3, 4
 	// parent - 0, 1, 2 от кого рождён атом? тепловой протон, pui1, pui2
-	double wr_ = wr * 0.97467943;   // Понижение wr для оттока части энергии в термальные протоны
+	double wr_ = wr * 0.97; // 0.97467943;   // Понижение wr для оттока части энергии в термальные протоны
 
 	int index = static_cast<int>(wr_ / phys_param->pui_wR * phys_param->pui_nW);
 	if (index < 0)  index = 0;
