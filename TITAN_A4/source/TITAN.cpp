79576d7b564c3310456a3454e872e47f370e5b7b
Sergey <korolkov.msu@mail.ru>
2026-06-05 00:52:58 +0300

    8.541

--- TITAN_A4/source/TITAN.cpp
+++ TITAN_A4/source/TITAN.cpp
@@ -208,13 +208,13 @@ int main()
 
 
     //S1.Algoritm(3, &S1);
-    S1.Algoritm(7, &S1);
+    //S1.Algoritm(7, &S1);
 
 
 
     //S1.Algoritm(1, &S1);
     //S1.Algoritm(5, &S1);
-    //S1.Algoritm(10, &S1);
+    S1.Algoritm(10, &S1);
     //S1.Algoritm(4, &S1);
     //S1.Algoritm(5, &S1);
     return 0;
